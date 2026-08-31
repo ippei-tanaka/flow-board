4c04c55a3329bcfde6f9a966a99d14af4509fd83
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2026-08-31 14:07:39 -0400

    Test

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -1,5 +1,7 @@
 import { createNeonAuth } from '@neondatabase/auth/next/server';
 
+console.log(process.env);
+
 export const auth = createNeonAuth({
   baseUrl: process.env.NEON_AUTH_BASE_URL!,
   cookies: {
